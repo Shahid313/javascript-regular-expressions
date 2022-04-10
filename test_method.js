fc28c490ec61759f5dc98eb4b293b7096db0cf85
Shahid Khan <shahidkahn11@gmail.com>
2022-04-10 11:56:08 +0500

    first commit

---
+++ test_method.js
@@ -0,0 +1,36 @@
+//--------------includes keyword------------------
+
+// let sentence = "Shahid Khan is learning programming"
+// let result = sentence.includes("Shahid")
+
+// if(result == true){
+//     console.log("Yes, Shahid is learning")
+// }else{
+//     console.log("No, he is not mentioned in the sentence")
+// }
+
+//--------------test keyword------------------
+
+// let sentence2 = "I am going to school"
+// result2 = /going/.test(sentence2)
+
+// if(result2 == true){
+//     console.log("Yes, he is going to school")
+// }else{
+//     console.log("No, he is not going to school")
+// }
+
+//--------------test method on array------------------
+
+// let words = ['swat', 'swabi', 'swastu', 'quetta']
+
+// words.filter((w,i) => {
+//     let first_three_letters = /swa/.test(w)
+//     if(first_three_letters){
+//         console.log(i+1+") "+w)
+//     }
+// })
+
+
+
+
